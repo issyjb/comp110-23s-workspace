26e58642e84284dbe40bacbd2d6f3b6809187aae
Isabelle Bolmer <issyjb@unc.edu>
2023-01-24 20:04:43 -0500

    EX01 Progress

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,21 @@
+"""EX01 - Chardle - A cute step toward World"""
+
+__author__ = "730557985"
+
+word_inputted: str = input("Enter a 5-character word: ")
+letter_inputted: str = input("Enter a single character: ")
+print("Searching for " + letter_inputted + " in " + word_inputted)
+count: int = 0
+
+sum(
+if (word_inputted[0] == letter_inputted):
+    print(letter_inputted + " found at index 0")
+if (word_inputted[1] == letter_inputted):
+    print(letter_inputted + " found at index 1")
+if (word_inputted[2] == letter_inputted):
+    print(letter_inputted + " found at index 2")
+if (word_inputted[3] == letter_inputted):
+    print(letter_inputted + " found at index 3")
+if (word_inputted[4] == letter_inputted):
+    print(letter_inputted + " found at index 4" ))
+Print("There are " + count + " instances of")
