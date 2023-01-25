c966c7313a36ed77f993d1c8178f904237ec1da4
Isabelle Bolmer <issyjb@unc.edu>
2023-01-24 20:23:18 -0500

    ex-1 cjamges

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -7,15 +7,23 @@ letter_inputted: str = input("Enter a single character: ")
 print("Searching for " + letter_inputted + " in " + word_inputted)
 count: int = 0
 
-sum(
 if (word_inputted[0] == letter_inputted):
     print(letter_inputted + " found at index 0")
+    count == (count + 1)
 if (word_inputted[1] == letter_inputted):
     print(letter_inputted + " found at index 1")
+    count == (count + 1)
 if (word_inputted[2] == letter_inputted):
     print(letter_inputted + " found at index 2")
+    count == (count + 1)
 if (word_inputted[3] == letter_inputted):
     print(letter_inputted + " found at index 3")
+    count == (count + 1)
 if (word_inputted[4] == letter_inputted):
-    print(letter_inputted + " found at index 4" ))
-Print("There are " + count + " instances of")
+    print(letter_inputted + " found at index 4" )
+    count == (count + 1)
+
+print("There are " + str(count) + " instances of " + letter_inputted)
+
+if count == 0:
+    print("No instances of " + letter_inputted + " found in " + word_inputted)
